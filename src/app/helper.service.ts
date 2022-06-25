ad3a144f92325bcc93b3f92b70f81fe8b8fae6dd
Romb3r <oliver.ulrich00@gmail.com>
2022-06-25 13:19:05 +0200

    edit header

--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -9,14 +9,17 @@ import { HttpClient, HttpResponse , HttpHeaders} from '@angular/common/http';
 })
 
 export class HelperService {
+  public code: string = "";
 
   constructor(private http: HttpClient) {
   }
 
   public postItem(postData: object) {
+    console.log(this.code)
     return this.http.post('https://ulv-api.fly.dev/v1/items', postData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -25,7 +28,8 @@ export class HelperService {
   public patchItem(patchData: object, uuid: string) {
     return this.http.patch('https://ulv-api.fly.dev/v1/items/' + uuid, patchData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -34,7 +38,8 @@ export class HelperService {
   public deleteItem(uuid: string) {
     return this.http.delete('https://ulv-api.fly.dev/v1/items/' + uuid, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -43,7 +48,8 @@ export class HelperService {
   public postPlace(postData: object) {
     return this.http.post('https://ulv-api.fly.dev/v1/places', postData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -52,7 +58,8 @@ export class HelperService {
   public patchPlace(patchData: object, uuid: string) {
     return this.http.patch('https://ulv-api.fly.dev/v1/places/' + uuid, patchData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -61,7 +68,8 @@ export class HelperService {
   public deletePlace(uuid: string) {
     return this.http.delete('https://ulv-api.fly.dev/v1/places/' + uuid, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -70,7 +78,8 @@ export class HelperService {
   public postCartItem(postData: object) {
     return this.http.post('https://ulv-api.fly.dev/v1/cart-items', postData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -79,7 +88,8 @@ export class HelperService {
   public patchCartItem(patchData: object, uuid: string) {
     return this.http.patch('https://ulv-api.fly.dev/v1/cart-items/' + uuid, patchData, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -88,7 +98,8 @@ export class HelperService {
   public deleteCartItem(uuid: string) {
     return this.http.delete('https://ulv-api.fly.dev/v1/cart-items/' + uuid, {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()
@@ -97,7 +108,8 @@ export class HelperService {
   public putCartItem(uuid: string) {
     return this.http.put('https://ulv-api.fly.dev/v1/cart-items/' + uuid + '/shopped', {
       headers: new HttpHeaders({
-        "Authorization": "Basic " + btoa("ulv:ulvistgeil")
+        "Authorization": "Basic " + btoa("ulv:ulvistgeil"),
+        "Code": this.code
       })
     })
     .toPromise()  
